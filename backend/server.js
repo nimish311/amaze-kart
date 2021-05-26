a120cbcdd15d2f1e3e25a8b4195bbb1c04586ea0
Nimish Gupta <guptanimish1998@gmail.com>
2021-05-26 12:58:41 +0530

    mongodb connected... no commit in frontend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 // Install babel packages bcs node only understands ES5 version and our code is in ES6, to convert ES6 to ES5 we need babel
 import express from 'express';
 import data from './data';
-const app = express();
+import dotenv from '../frontend/node_modules/dotenv';
+import mongoose from 'mongoose';
+import config from './config';
+import userRoutes from './routes/userRoutes';
 
+const app = express();
+dotenv.config();
+const mongodbURL = config.MONGODB_URL;
+
+mongoose
+  .connect(mongodbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error.reason));
+
+app.use('/api/users', userRoutes);
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
